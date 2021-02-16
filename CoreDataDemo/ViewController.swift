3c7d9b4c1ccd1b22b75af30e8f31c81b75576a1b
TristaWu <trista@kcdgs.com>
2021-02-16 16:38:23 +0800

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Trista on 2021/2/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
